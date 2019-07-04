f8977685c391cfd450733a3e9f3b8d776866760a
Dmytro Frolov1 <Dmytro_Frolov1@epam.com>
2019-07-04 16:36:55 +0300

    Webpack file loader installed

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,12 @@ module.exports = {
                 test: /\.pug$/,
                 use: ["pug-loader"]
             },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
         ]
     },
     plugins: [
