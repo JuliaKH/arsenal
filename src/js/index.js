728ac25ca781564e753366a6e1969fa074824781
dmitrolov <dmitrolov@gmail.com>
2019-07-07 19:16:41 +0300

    Header rewrited

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,5 @@
-import jQuery from 'jquery';
-import foundation from 'foundation-sites';
+import $ from 'jquery';
+import 'what-input';
+import 'foundation-sites';
 
-// import '../sass/base.sass';
-// import 'vendor/jquery.js';
-// import 'vendor/what-input.js';
-// import 'vendor/foundation.js';
-// const proc = require('process');
-// console.log(proc);
 $(document).foundation();
