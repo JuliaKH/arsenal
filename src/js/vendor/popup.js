1179c5d186543552df697ab0cbd6bcc740abdfe8
Yuliia Khalina <Yuliia_Khalina@epam.com>
2019-07-09 13:52:11 +0300

    added registration form

--- src/js/vendor/popup.js
+++ src/js/vendor/popup.js
@@ -8,18 +8,25 @@ let btn = document.getElementById("myBtn");
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-};
+btn.onclick = () => modal.style.display = "block";
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
+span.onclick = () => modal.style.display = "none";
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 } ;
+
+let regBtn = document.getElementsByClassName('registration')[0];
+let submitBtn = document.getElementsByClassName('submit')[0];
+let url_input = document.getElementsByClassName('url')[0];
+let heading = document.getElementsByClassName('heading')[0];
+
+regBtn.onclick = () => {
+    url_input.style.display = "block";
+    heading.innerHTML = 'Registration';
+    submitBtn.innerHTML = 'Register';
+};
