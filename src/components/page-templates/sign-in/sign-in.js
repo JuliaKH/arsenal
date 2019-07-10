aa6f1fc3f6ff5fb7153abf27386067e01cb3e873
Yuliia Khalina <Yuliia_Khalina@epam.com>
2019-07-10 18:01:22 +0300

    added firebase

--- src/components/page-templates/sign-in/sign-in.js
+++ src/components/page-templates/sign-in/sign-in.js
@@ -21,6 +21,33 @@ const email = document.querySelector('.email');
 const password = document.getElementsByClassName('psw')[0];
 const url = document.querySelector('.url').value;
 
+submitBtn.addEventListener('click',(event) => {
+    let isRegistered = false;
+    //Log in
+    if(event.target.classList.contains('login')){
+        registeredUsers.forEach(user => {
+            console.log(email.value ,password.value);
+            if(user.login === email.value &&  user.password === password.value) {
+                console.log('ok');
+                isRegistered = true;
+            }
+        });
+        if(isRegistered === true)
+            document.getElementById('regForm').submit();
+        else alert('Login or password doesn`t fit. Register, please');
+    }
+    //Registration
+    if (event.target.classList.contains('registration')){
+        console.log('reg');
+        const obj = {};
+        obj.login = email.value;
+        obj.password = password.value;
+        obj.url = url;
+        registeredUsers.push(obj);
+        document.getElementById('regForm').submit();
+        console.log(registeredUsers);
+    }
+});
 submitBtn.onclick = (event) => {
     let isRegistered = false;
     //Log in
--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import $ from 'jquery';
 import 'what-input/dist/what-input.min';
 import 'foundation-sites/dist/js/foundation';
 import 'foundation-sites/dist/css/foundation.css';
+import './firebase-config'
 
+var firebase = require('firebase/app');
 
+require('firebase/auth');
+require('firebase/database');
+require('firebase/storage');
 $(document).foundation();
