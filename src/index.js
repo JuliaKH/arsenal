39bfd51446e87ca60352ce140db957634c67aaaa
Yuliia Khalina <Yuliia_Khalina@epam.com>
2019-07-11 12:38:07 +0300

    added hosting

--- src/index.js
+++ src/index.js
@@ -11,3 +11,9 @@ import 'foundation-sites/dist/css/foundation.css';
 
 
 $(document).foundation();
+
+var firebase = require('firebase/app');
+
+require('firebase/auth');
+require('firebase/database');
+require('firebase/storage');
