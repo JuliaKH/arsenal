0ea6def4350aea492628385e07655fc893b06b87
dmitrolov <nunky.balthazar@gmail.com>
2019-07-15 14:29:45 +0300

    Classes improvements (#59)
    * Close function developed
    
    * Popup class improvements

---
+++ src/components/sign-in/variables.js
@@ -0,0 +1,14 @@
+const signInDom = {
+    openSignInFormButton: document.querySelector('.authorization-button'),
+    userAvatarImage: document.querySelector('.avatar'),
+    // Sign-in
+    signInFormContainer: document.querySelector('.sign-in-form-container'),
+    signInCloseFormElement: document.querySelector('.sign-in__close-form'),
+    switchToRegistrationButton: document.querySelector('.switch-to-registration'),
+    // Sign-out
+    registrationFormContainer: document.querySelector('.registration-form-container'),
+    registrationCloseFormElement: document.querySelector('.registration__close-form'),
+    switchToSignIn: document.querySelector('.switch-to-sign-in'),
+};
+
+export {signInDom}
