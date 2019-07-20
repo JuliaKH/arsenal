9fe4bff720d12341adbb817026045547c7d6fb70
JuliaKH <43015141+JuliaKH@users.noreply.github.com>
2019-07-20 16:16:52 +0300

    deleted observable test

--- src/components/sign-in/google-sign-in/Observable.spec.js
+++ src/components/sign-in/google-sign-in/Observable.spec.js
@@ -1,9 +1,9 @@
-import  SignInObservable  from './Observable';
-
-describe("should return 10", () => {
-    it("some number", () => {
-        let obs = new SignInObservable();
-        let sum = obs.sum(5,5);
-        expect(sum()).toEqual(10);
-    });
-});
+// import  SignInObservable  from './Observable';
+//
+// describe("should return 10", () => {
+//     it("some number", () => {
+//         let obs = new SignInObservable();
+//         let sum = obs.sum(5,5);
+//         expect(sum()).toEqual(10);
+//     });
+// });
